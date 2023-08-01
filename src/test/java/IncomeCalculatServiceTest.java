fd54d8303ac153c7a6fcaac9c08c6c2e484b3f84
Sergey Zagrebelny <zagrebelny.s.v@gmail.com>
2023-08-01 23:17:52 +0300

    ParameterizedTest

--- src/test/java/IncomeCalculatServiceTest.java
+++ src/test/java/IncomeCalculatServiceTest.java
@@ -1,21 +1,27 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa67.IncomeCalculator.service.IncomeCalculatService;
 
 public class IncomeCalculatServiceTest {
-    @Test
-    public void calcFor10_000(){
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void calcFor10_000(int expected, int income, int expenses, int threshold){
         IncomeCalculatService service =new IncomeCalculatService();
-        int expected = 3;
-        int actual = service.incomeCalc(10_000, 3_000, 20_000);
+//        int expected = 3;
+        int actual = service.incomeCalc(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void calcFor100_000(){
-        IncomeCalculatService service =new IncomeCalculatService();
-        int expected = 2;
-        int actual = service.incomeCalc(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void calcFor100_000(){
+//        IncomeCalculatService service =new IncomeCalculatService();
+//        int expected = 2;
+//        int actual = service.incomeCalc(100_000, 60_000, 150_000);
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
