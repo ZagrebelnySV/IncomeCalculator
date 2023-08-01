9c8e80c02fa362ea24446a81828b2dc179902dae
Sergey Zagrebelny <zagrebelny.s.v@gmail.com>
2023-08-01 23:35:01 +0300

    ParameterizedTest with csv-fele

--- src/test/java/IncomeCalculatServiceTest.java
+++ src/test/java/IncomeCalculatServiceTest.java
@@ -1,15 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa67.IncomeCalculator.service.IncomeCalculatService;
 
 public class IncomeCalculatServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files = "src/test/resources/IncomeCalculat.csv")
     public void calcFor10_000(int expected, int income, int expenses, int threshold){
         IncomeCalculatService service =new IncomeCalculatService();
 //        int expected = 3;
